043df6292bdbcced57ca5a94f7dc56bc9bcc7fde
Emre Ozdemir <emrozdmr55@gmail.com>
2016-01-03 15:33:01 +0200

    Base version

---
+++ Number To Words/WindowsFormsApplication8/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WindowsFormsApplication8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numLabel = new System.Windows.Forms.Label();
+            this.numBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // numLabel
+            // 
+            this.numLabel.AutoSize = true;
+            this.numLabel.Location = new System.Drawing.Point(12, 38);
+            this.numLabel.Name = "numLabel";
+            this.numLabel.Size = new System.Drawing.Size(84, 13);
+            this.numLabel.TabIndex = 0;
+            this.numLabel.Text = "Enter a Number.";
+            // 
+            // numBox
+            // 
+            this.numBox.Location = new System.Drawing.Point(12, 15);
+            this.numBox.MaxLength = 6;
+            this.numBox.Name = "numBox";
+            this.numBox.Size = new System.Drawing.Size(101, 20);
+            this.numBox.TabIndex = 1;
+            this.numBox.TextChanged += new System.EventHandler(this.numBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 94);
+            this.Controls.Add(this.numBox);
+            this.Controls.Add(this.numLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label numLabel;
+        private System.Windows.Forms.TextBox numBox;
+    }
+}
+
---
+++ Number To Words/WindowsFormsApplication8/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] NumArray = {" ","one","two","three","four","five","six","seven","eight","nine", //0-9
+                                "ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen", //11-19
+                                "twenty","thirty","forty","fifty","sixty","seventy","eighty","ninty","hundred"}; //20-30-40...-90
+
+        public static string NumberToWords(int number)
+        {
+            if (number == 0)
+                return "zero";
+
+            if (number < 0)
+                return "minus " + NumberToWords(Math.Abs(number));
+
+            string words = "";
+
+            if ((number / 1000000) > 0)
+            {
+                words += NumberToWords(number / 1000000) + " million ";
+                number %= 1000000;
+            }
+
+            if ((number / 1000) > 0)
+            {
+                words += NumberToWords(number / 1000) + " thousand ";
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words += NumberToWords(number / 100) + " hundred ";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words != "")
+                    words += "and ";
+
+                var unitsMap = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+                var tensMap = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+                if (number < 20)
+                    words += unitsMap[number];
+                else
+                {
+                    words += tensMap[number / 10];
+                    if ((number % 10) > 0)
+                        words += "-" + unitsMap[number % 10];
+                }
+            }
+
+            return words;
+        }
+        
+        
+        private void numBox_TextChanged(object sender, EventArgs e)
+        {
+
+#region previous code
+            string strNumber = numBox.Text.ToString(); //Number as a string
+            int intNumber = 0;                         //Number as an integer
+
+            if (strNumber.Length > 0) //Convert strNumber to int if there is a number
+                intNumber = Convert.ToInt32(numBox.Text.ToString());
+
+
+            else if (strNumber.Length == 0) //No number entered or number deleted.
+                numLabel.Text = "Enter a number.";
+
+
+            numLabel.Text = NumberToWords(intNumber);
+
+            //if (strNumber.Length <= 6 && strNumber.Length > 0) // Number length between 1-6
+            //{
+            //    int sixthPlace = intNumber / 100000;            //_xx xxx
+            //    int fifthPlace = (intNumber % 100000) / 10000;  //x_x xxx
+            //    int fourthPlace = (intNumber % 10000) / 1000;   //xx_ xxx
+            //    int thirdPlace = (intNumber % 1000) / 100;      //xxx _xx
+            //    int secondPlace = (intNumber % 100) / 10;       //xxx x_x
+            //    int firstPlace = intNumber % 10;                //xxx xx_
+
+
+            //    if (intNumber >= 0 && intNumber <= 20)                  // 0-20
+            //        numLabel.Text = NumArray[intNumber];
+
+            //    else if (intNumber >= 21 && intNumber <= 99)            // 21-99
+            //        numLabel.Text = NumArray[secondPlace + 18] + " " + NumArray[firstPlace];
+
+            //    else if (intNumber >= 100 && intNumber <= 999)          // 100 - 999
+            //    {
+            //        if (secondPlace == 0 ) //100-109/200-209/300-309.../900-909
+            //            numLabel.Text = NumArray[thirdPlace] + " hundred " + NumArray[firstPlace];
+
+            //        else if (secondPlace == 1)//110-119/210-219.../910-919
+            //            numLabel.Text = NumArray[thirdPlace] + " hundred " + NumArray[firstPlace + 10];
+
+            //        else if (secondPlace >= 2)   //120-199/220-299/320-399.../920-999
+            //            numLabel.Text = NumArray[thirdPlace] + " hundred " + NumArray[secondPlace + 18] + " " + NumArray[firstPlace % 10];
+
+            //    }
+
+            //    else if (intNumber >= 1000 && intNumber <=9999)          //1000-9999
+            //    {
+            //        numLabel.Text = NumArray[fourthPlace] + " thousand"+ NumArray[thirdPlace] + " hundred " + NumArray[secondPlace + 18] + " " + NumArray[firstPlace % 10];
+            //    }
+            //}
+
+
+#endregion
+        }
+
+
+    }
+}
